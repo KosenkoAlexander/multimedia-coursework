ca0d5d9c7b77f0b662070f8858352363fc59fee3
Gleb-05 <gms.4job@gmail.com>
2025-11-25 09:21:08 +0000

    stt draft

--- README.md
+++ README.md
@@ -13,3 +13,6 @@ import nltk
 nltk.download('averaged_perceptron_tagger_eng')
 nltk.download('punkt_tab')
 ```
+
+# Text and speech
+https://github.com/espnet/espnet_model_zoo
--- src/text_and_speech/stt.py
+++ src/text_and_speech/stt.py
@@ -1,1 +1,14 @@
-# espnet should have models for speech-to-text
+import soundfile
+from espnet2.bin.asr_inference import Speech2Text
+# from espnet2.bin.s2t_inference import Speech2Text  # for newer models
+
+# HOWTO
+# https://github.com/espnet/espnet_model_zoo
+
+model = Speech2Text.from_pretrained("kamo-naoyuki/wsj")  # it took FOREVER
+
+speech, rate = soundfile.read("tmp/response.wav")
+nbests = model(speech)
+
+text, *_ = nbests[0]
+print(text)
